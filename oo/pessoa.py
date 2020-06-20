f52c670e98acd2a26e6ca1d7f64fcf33ca841f22
Joao Paulo Limberger - SHOO <jlimberger@gmail.com>
2020-06-20 13:41:20 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
 
-    def __init__(self, nome=None, idade=51):
+    def __init__(self, *filhos, nome=None, idade=51):
         self.idade = idade
         self.nome = nome
+        self.filhos  = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Joao')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome='Jotapê'
-    print(p.nome)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
