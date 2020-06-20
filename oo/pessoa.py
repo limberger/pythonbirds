f19af56c7d79d70181e081549eb9f69f64e22fd1
Joao Paulo Limberger - SHOO <jlimberger@gmail.com>
2020-06-20 13:37:00 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+
+    def __init__(self, nome=None, idade=51):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Joao')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome='Jotapê'
+    print(p.nome)
